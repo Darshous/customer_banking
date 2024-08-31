044c69bbf22155a90baf0ca1ad572e82c168fb32
Darshous <johnathan.marsh01@gmail.com>
2024-08-31 12:53:18 -0400

    Updating to the cd_account.py Adding required code.

--- M3_Starter_Code/cd_account.py
+++ M3_Starter_Code/cd_account.py
@@ -1,5 +1,7 @@
 """Import the Account class from the Account.py file."""
 # ADD YOUR CODE HERE
+from Account import Account
+
 
 def create_cd_account(balance, interest_rate, months):
     """Creates a CD account, calculates interest earned, and updates the account balance.
@@ -15,19 +17,19 @@ def create_cd_account(balance, interest_rate, months):
     """
     # Create an instance of the `Account` class and pass in the balance and interest parameters.
     #  Hint: You need to add the interest as a value, i.e, 0.
-    # ADD YOUR CODE HERE
-
+    # ADD YOUR CODE HERE # similar to the savings_account requirement when instancing the Account class
+    cd_account = Account(balance, 0)
     # Calculate interest earned
-    # ADD YOUR CODE HERE
-
+    # ADD YOUR CODE HERE # using same code from savings_account
+    interest = balance * (interest_rate / 100) * (months / 12)
     # Update the CD account balance by adding the interest earned
+    # ADD YOUR CODE HERE # Same code as savings_account
+    updated_balance = balance + interest
+    # Pass the updated_balance to the set balance method using the instance of the CDAccount class.  #?? should this be CD_account variable rather than class?
     # ADD YOUR CODE HERE
-
-    # Pass the updated_balance to the set balance method using the instance of the CDAccount class.
+    cd_account.set_balance(updated_balance)
+    # Pass the interest_earned to the set interest method using the instance of the CDAccount class. #?? should this be CD_account variable rather than class?
     # ADD YOUR CODE HERE
-
-    # Pass the interest_earned to the set interest method using the instance of the CDAccount class.
-    # ADD YOUR CODE HERE
-
+    cd_account.set_interest(interest)
     # Return the updated balance and interest earned.
-    return  # ADD YOUR CODE HERE
+    return updated_balance, interest  # ADD YOUR CODE HERE
